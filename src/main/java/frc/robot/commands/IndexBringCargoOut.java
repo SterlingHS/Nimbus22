336cf0e9a5def6f757b51d6382081e6f83c1d8a2
Patrick Riley <spikesriley@gmail.com>
2022-02-12 08:52:28 -0600

    Added the IndexOut command to the last branch

--- src/main/java/frc/robot/commands/IndexBringCargoOut.java
+++ src/main/java/frc/robot/commands/IndexBringCargoOut.java
@@ -35,7 +35,7 @@ public class IndexBringCargoIn extends CommandBase {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_index.cargo_index_in();
+        m_index.cargo_index_out();
     }
     
     // Called once the command ends or is interrupted.
