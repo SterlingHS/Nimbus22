9dca3efa0918e9cd885cff3b02bdc84c4e0118b8
Alexis Rivera <poteus@gmail.com>
2022-03-03 13:51:38 -0600

    Added method is_speed_reached to SmartShooterPID

--- src/main/java/frc/robot/commands/SmartShooterPID.java
+++ src/main/java/frc/robot/commands/SmartShooterPID.java
@@ -3,6 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
+import frc.robot.RobotMap;
 import frc.robot.subsystems.Index;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
@@ -13,9 +14,10 @@ public class SmartShooterPID extends CommandBase {
   private final Shooter m_shooter;
   private final Limelight m_limelight;
   private final Index m_index;
+  private double speed;
 
   /** Creates a new SmartShooter. */
-  public SmartShooterPID(Shooter subsystem1, Limelight subsystem2, Index subsystem3) {
+  public SmartShooterPID(Shooter subsystem1, Limelight subsystem2, Index subsystem3, double spd) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_shooter = subsystem1;
     addRequirements(m_shooter);
@@ -23,6 +25,7 @@ public class SmartShooterPID extends CommandBase {
     addRequirements(m_limelight);
     m_index = subsystem3;
     addRequirements(m_index);
+    speed = spd;
   }
 
   // Called when the command is initially scheduled.
@@ -33,7 +36,7 @@ public class SmartShooterPID extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_shooter.shootCargoPID(6000); // Test to shoot to 6000 of "speed"
+    m_shooter.shootCargoPID(speed); // Test to shoot to 6000 of "speed"
   }
 
   // Called once the command ends or is interrupted.
@@ -46,6 +49,15 @@ public class SmartShooterPID extends CommandBase {
   @Override
   public boolean isFinished() {
     return false;
+  }
+
+  public boolean is_speed_reached(){
+    double current_speed = m_shooter.read_speed_shooter();
+    if(current_speed > RobotMap.SHOOT_CARGO_SPEED*(1+RobotMap.DISTANCE_ACCURACY) 
+          && current_speed < RobotMap.SHOOT_CARGO_SPEED*(1+RobotMap.DISTANCE_ACCURACY))
+            return true;
+          
+    return false;  
   }
 
 }
