c48bb0428cc6bee72c24c278b98c58e850914d77
Alexis Rivera <poteus@gmail.com>
2022-03-24 13:49:58 -0500

    Cleaned Systems

--- src/main/java/frc/robot/commands/SmartShooter.java
+++ src/main/java/frc/robot/commands/SmartShooter.java
@@ -1,14 +1,9 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
 import frc.robot.RobotMap;
-//mport frc.robot.RobotMap;
 import frc.robot.subsystems.Index;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
-//import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class SmartShooter extends CommandBase {
