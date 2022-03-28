1f4a9989aabd45a7eaf20cfb81a7e0bbf91670b1
Alexis Rivera <poteus@gmail.com>
2022-03-28 15:12:57 -0500

    Finished TurnAngle

--- src/main/java/frc/robot/commands/TurnAngle.java
+++ src/main/java/frc/robot/commands/TurnAngle.java
@@ -11,6 +11,7 @@ public class TurnAngle extends CommandBase {
 
   DriveSystem drivesystem;
   private static double angle;
+  private static double startingAngle;
 
   public TurnAngle(DriveSystem subs, double angle1) {
     drivesystem = subs;
@@ -24,21 +25,30 @@ public class TurnAngle extends CommandBase {
 
   // Called just before this Command runs the first time
   @Override
-  public void initialize() {}
+  public void initialize() {
+    startingAngle = drivesystem.getAngle();
+  }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  public void execute() {}
+  public void execute() {
+    if (angle < 0) drivesystem.turnLeft();
+    else           drivesystem.turnRight();
+  
+  }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   public boolean isFinished() {
+    if (angle >= 0 && angle + startingAngle < drivesystem.getAngle()) return true;
+    if (angle <= 0 && angle + startingAngle > drivesystem.getAngle()) return true;
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
   public void end(boolean interrupted){
+    drivesystem.stop();
   }
 
 }
