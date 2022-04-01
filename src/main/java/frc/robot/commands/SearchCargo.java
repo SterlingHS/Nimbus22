db510a90999866ae14a8e2bfceb37b764a2c1287
Alexis Rivera <poteus@gmail.com>
2022-04-01 17:45:44 -0500

    Split SearchCargo into 3 stages

--- src/main/java/frc/robot/commands/SearchCargo.java
+++ src/main/java/frc/robot/commands/SearchCargo.java
@@ -1,5 +1,6 @@
 
 package frc.robot.commands;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSystem;
@@ -18,7 +19,8 @@ import frc.robot.subsystems.Pixie;
     private final Intake m_intake;
     private final Index m_index;
 
-    private static boolean center;
+    private static int stage;
+    private static int alliance; // True = Blue, False = Red
  
     public SearchCargo(Pixie sub2, DriveSystem sub3, Intake sub4, Index sub5) {
         drivesystem = sub3;
@@ -36,6 +38,9 @@ import frc.robot.subsystems.Pixie;
     @Override
     public void initialize() {
         center = false;
+        stage = 1;
+        if (DriverStation.getAlliance() == DriverStation.Alliance.Blue) alliance = 1;
+        else alliance = 2;
     }
 
     // Called every time the scheduler runs while the command is scheduled.
@@ -51,7 +56,9 @@ import frc.robot.subsystems.Pixie;
         SmartDashboard.putNumber("Pixie dimx", m_pixie.Read_Pixy_dimx());
         SmartDashboard.putNumber("Pixie dimy", m_pixie.Read_Pixy_dimy());
 
-        stage1();
+        if(stage == 1) stage1(); // Find ball
+        if(stage == 2) stage2(); // Go to ball
+        if(stage == 3) stage3(); // Go forward until we pick up ball
     }
 
     // Called once the command ends or is interrupted.
@@ -66,8 +73,7 @@ import frc.robot.subsystems.Pixie;
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        if (m_index.is_cargo_in_index())
-        System.out.println("Interupted");
+        if (m_index.is_cargo_in_index()) System.out.println("Interupted");
         return m_index.is_cargo_in_index();
     }
 
@@ -79,42 +85,37 @@ import frc.robot.subsystems.Pixie;
 
     private void stage1()
     {
-        if(m_pixie.Read_Pixy_is_Ball() == false && center == false) {
-            drivesystem.turnRight();
-            m_intake.cargointake_stop();
-            m_index.index_stop();
-        }
-        else
-        {
-            double x = m_pixie.Read_Pixy_x() - 240;
+        drivesystem.turnRight();
+        m_intake.cargointake_stop();
+        m_index.index_stop();
+        if(m_pixie.Read_Pixy_is_Ball() == true && m_pixie.Read_Pixy_signature() == alliance) stage = 2;
+    }
 
-            // If the ball is in sight then go forward
-            if(-20 < x && x < 20 || center == true){
-                double turn = 0;
-                System.out.println("x: " + x);
-                if(x <= 10) turn = -.15;
-                if(x > 10) turn = .15;
-                drivesystem.mecanumDrive(0 , -.35, turn, 1);
-                m_intake.cargointake();
-                m_index.cargo_index_in();
-                center = true;
-            }
-            else {
-                if(x <= 20){
-                    drivesystem.turnLeft();
-                    m_intake.cargointake_stop();
-                    m_index.index_stop();
-                    center = false;
-                    // System.out.println("left x: " + x);
-                }
-                else 
-                {
-                    drivesystem.turnRight();
-                    m_intake.cargointake_stop();
-                    m_index.index_stop();
-                    center = false;  
-                }
-            } 
-        }
+    private void stage2()
+    {
+        double x = m_pixie.Read_Pixy_x() - 240;
+
+        if(x <= 20) drivesystem.turnLeft();
+        else        drivesystem.turnRight();
+            
+        m_intake.cargointake_stop();
+        m_index.index_stop();
+        if(-20 < x && x < 20) stage = 3;
+    }
+
+    private void stage3()
+    {
+        double x = m_pixie.Read_Pixy_x() - 240;
+
+        // If the ball is in sight then go forward
+        double turn = 0;
+        double forward = -.20;
+        //System.out.println("x: " + x);
+        if(x <= 10) turn = -.15;
+        if(x > 10)  turn = .15;
+        if(m_pixie.Read_Pixy_is_Ball()) forward = -0.35;
+        drivesystem.mecanumDrive(0 , forward, turn, 1);
+        m_intake.cargointake();
+        m_index.cargo_index_in(); 
     }
 }
